5da8c12df8bb63a9aeb0df53da32723e5b43dbd2
broCapang <U2000429@siswa.um.edu.my>
2023-04-04 06:40:01 +0800

    done all the code

--- ALgone/labViva1.py
+++ ALgone/labViva1.py
@@ -37,29 +37,76 @@ def radix_sort(arr):
         
     return arr
 
-def rabin_karp(text, pattern):
-    # Choose a prime number as the base for the hash function
-    prime = 31
+def rabin_karp_hash(s, a=101, q=10**9+9):
+    n = len(s)
+    h = 0
+    for i in range(n):
+        h = (h * a + ord(s[i])) % q
+    return h
+
+def rabin_karp(text, pattern, a=101, q=10**9+9):
+    n = len(text)
+    m = len(pattern)
+    pattern_hash = rabin_karp_hash(pattern, a, q)
+    text_hash = rabin_karp_hash(text[:m], a, q)
+    for i in range(n - m + 1):
+        if text_hash == pattern_hash:
+            if text[i:i+m] == pattern:
+                return i
+        if i < n - m:
+            text_hash = (text_hash - ord(text[i]) * pow(a, m-1, q)) % q
+            text_hash = (text_hash * a + ord(text[i+m])) % q
+    return -1
+
+def kmp(text, pattern):
+    n = len(text)
+    m = len(pattern)
     
-    # Calculate the hash value of the pattern
-    p_hash = 0
-    for char in pattern:
-        p_hash = p_hash * prime + ord(char)
-        
-    # Create a rolling hash for the text
-    s_hash = 0
-    for i in range(len(pattern)):
-        s_hash = s_hash * prime + ord(text[i])
-        
-    # Check if the hash values match and compare substrings
-    matches = []
-    for i in range(len(text) - len(pattern) + 1):
-        if s_hash == p_hash and text[i:i+len(pattern)] == pattern:
-            matches.append(i)
-        if i < len(text) - len(pattern):
-            s_hash = s_hash - ord(text[i]) * (prime**(len(pattern)-1))
-            s_hash = s_hash * prime + ord(text[i+len(pattern)])
-    return matches
+    # Build the prefix function
+    prefix = [0] * m
+    j = 0
+    for i in range(1, m):
+        while j > 0 and pattern[j] != pattern[i]:
+            j = prefix[j-1]
+        if pattern[j] == pattern[i]:
+            j += 1
+        prefix[i] = j
+    
+    # Use the prefix function to search for the pattern in the text
+    j = 0
+    for i in range(n):
+        while j > 0 and pattern[j] != text[i]:
+            j = prefix[j-1]
+        if pattern[j] == text[i]:
+            j += 1
+        if j == m:
+            return i - m + 1
+    return -1
+
+def finite_automata(text, pattern):
+    n = len(text)
+    m = len(pattern)
+    
+    # Build the transition function for the pattern
+    transitions = {0: {pattern[0]: 1}, 1: {}}
+    j = 0
+    for i in range(1, m):
+        for c in set(pattern[:i+1]):
+            if c not in transitions[i]:
+                if pattern[:i] + c in pattern[:i+1]:
+                    transitions[i][c] = i + 1
+                else:
+                    transitions[i][c] = 0
+        j = transitions[j].get(pattern[i], 0)
+        transitions[i+1] = {}
+    
+    # Use the transition function to search for the pattern in the text
+    j = 0
+    for i in range(n):
+        j = transitions[j].get(text[i], 0)
+        if j == m:
+            return i - m + 1
+    return -1
 
 
 if __name__ == "__main__":
@@ -69,6 +116,20 @@ if __name__ == "__main__":
     print(sorted_arr)
     print(radix_arr)
     text = "timecomplexityisfunalgorithmisfun"
-    patterns = ["algorithm", "fun"]
-    result = rabin_karp(text, patterns)
-    print(result)
+    pattern = "algorithm"
+    index = rabin_karp(text, pattern)
+    if index == -1:
+        print("Pattern not found")
+    else:
+        print("Pattern found at index", index)
+    index = kmp(text, pattern)
+    if index == -1:
+        print("Pattern not found")
+    else:
+        print("Pattern found at index", index)
+    index = finite_automata(text, pattern)
+    if index == -1:
+        print("Pattern not found")
+    else:
+        print("Pattern found at index", index)
+
